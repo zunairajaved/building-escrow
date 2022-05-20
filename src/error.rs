199e295712bf1ca3f2eb571f8a4d19651cacbce5
zunairajaved <zunaira.javed@invozone.com>
2022-05-20 18:09:03 +0500

    building escrow

---
+++ src/error.rs
@@ -0,0 +1,20 @@
+// inside error.rs
+use thiserror::Error;
+
+use solana_program::program_error::ProgramError;
+
+use crate::instruction::EscrowInstruction;
+use crate::{instruction::EscrowInstruction, error::EscrowError};
+
+#[derive(Error, Debug, Copy, Clone)]
+pub enum EscrowError {
+    /// Invalid instruction
+    #[error("Invalid Instruction")]
+    InvalidInstruction,
+}
+
+impl From<EscrowError> for ProgramError {
+    fn from(e: EscrowError) -> Self {
+        ProgramError::Custom(e as u32)
+    }
+}
